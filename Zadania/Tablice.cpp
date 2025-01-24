e98422cf40ddfe77c33316d970f0fcb7064cc421
mika190 <nataliakropkap@gmail.com>
2025-01-24 12:03:31 +0100

    Dodałam temat tablic

---
+++ Zadania/Tablice.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main(){
+	//zad1
+	/*
+	int tablica [5];
+	int suma=0;
+	float srednia;
+		cout<<"podaj 5 liczb calkowitych: "<<endl;
+				for(int i=0;i<5;i++){
+				cout<<"podaj liczbe nr"<<i+1<<":";
+				cin>> tablica [i];
+				suma += tablica [i];
+				}
+		srednia= suma / 5.0;
+		cout<< "suma podanych liczb wynosi: "<<suma<<endl;
+		cout << "srednia liczb wynosi: "<<srednia<<endl;
+	*/
+		//zad2
+	int tablica [5];
+	cout<<"podaj 5 liczb calkowitych"<<endl;
+			for(int i=0;i<5;i++){
+			cout<<"podaj liczbe nr "<< i+1<<":";
+			cin>> tablica [i];
+				
+				
+				
+				int min=0;
+			}
+		//DO DOKONCZENIA 
+			
+		
+		
+		
+		
+    return 0;
+}
