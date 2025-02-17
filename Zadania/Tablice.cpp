40e1171ed7684cb2262d1c4d96c5071b4b8a95e7
mika190 <nataliakropkap@gmail.com>
2025-02-17 11:09:07 +0100

    dodałam kolejne zadania do tablic

--- Zadania/Tablice.cpp
+++ Zadania/Tablice.cpp
@@ -17,17 +17,43 @@ int main(){
 		cout << "srednia liczb wynosi: "<<srednia<<endl;
 	*/
 		//zad2
-	int tablica [5];
+		/*
+int tablica [5];
 	cout<<"podaj 5 liczb calkowitych"<<endl;
 			for(int i=0;i<5;i++){
 			cout<<"podaj liczbe nr "<< i+1<<":";
 			cin>> tablica [i];
-				
-				
-				
-				int min=0;
+			
 			}
-		//DO DOKONCZENIA 
+	cout << "Wprowadzone liczby to: "<<endl;
+   			 for (int i = 0; i < 5; i++) {
+      		  cout << tablica[i]<< " "<<endl;
+   			 }
+	cout<< "odwrotna kolejnosc"<<endl;
+		 for (int i = 4; i >= 0; i--){
+        cout << tablica[i] << " ";
+    }
+	*/
+	//zad3
+	int tablica [5];
+		cout<<"wprowadz 5 liczb dodatnich i ujemnych"<<endl;
+			for (int i=0;i <5;i++){
+				cout<<"podaj liczbe nr"<<i+1<<endl;
+				cin>> tablica [i];
+					if(tablica[i]<0){
+						tablica [i]=0;
+					}
+				}
+			 for (int i = 0; i < 5; i++) {
+       			 cout << tablica[i];
+       				 if (i < 4) {
+            		cout << ", ";
+       							 }
+				
+				}
+	
+	
+	
 			
 		
 		
