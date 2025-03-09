9db0e0d86bd1920fc850de772861cd85260d5799
Natalia <90264996+mikapl19@users.noreply.github.com>
2025-03-09 16:12:21 +0100

    Dodałam ostatnie zadanie po 4 miesiącach...⭐

--- Zadania/petla for.cpp
+++ Zadania/petla for.cpp
@@ -114,45 +114,95 @@ int mnozenie;
 			}
 */
 //zad6
-int klocek;
-int wysokosc;
-cout<< "podaj wysokosc: "<<endl;
-cin>> wysokosc;
-int szerokosc;
-cout<<"podaj szerokosc:"<< endl;
-cin>> szerokosc;
-	for (int w=0;w<=wysokosc;w++){
-		
-		
-			for (int s=0;s<=szerokosc;s++){
-			
-			if ()
-			cout<< "* ";
-				
-				
-			
-				//DOKONCZ TO COS Z IF XD
-				
-				
-				
-				
-			}
-		
-		cout<< endl;
-		
-		
-		
-		
-	}	
+/*
+   int wy, sz;  
+    cout << "Podaj liczbe szerokosci: ";
+    cin >> sz;
+    cout << "Podaj liczbe wysokosci: ";
+    cin >> wu;
 
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < s; j++) {
+            if (i == 0 || i == w - 1) {
+                cout << "*"; 
+            } 
+            else if (j == 0 || j == s - 1) {
+                cout << "*"; 
+            } 
+            else {
+                cout << " "; 
+            }
+        }
+        cout << endl; 
+    }
+*/
+//zad7
+/*
+int w;
+int s;
 
+    cout << "Podaj szerokosc: ";
+    cin >> sz;
+    cout << "Podaj wysokosc: ";
+    cin >> wy;
 
+    for (int i = 0; i < s; i++) {
+        cout << "-";
+    }
+    cout << endl;
 
+    for (int i = 1; i < w - 1; i++) {
+        cout << "|";
 
+        for (int j = 1; j < s - 1; j++) {
+            if ((i + j) % 2 == 0) {
+                cout << "#";
+            } else {
+                cout << "*";
+            }
+        }
 
+        cout << "|";
+        cout << endl;
+    }
 
+    for (int i = 0; i < s; i++) {
+        cout << "-";
+    }
+    cout << endl;
+*/
+//zad8
+/*
+int wy;
+int sz;
+    cout << "Podaj szerokosci: ";
+    cin >> sz;
+    cout << "Podaj wysokosci: ";
+    cin >> wy;
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < s - 2 * i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+*/
+//zad9
+    int basia = 0;
+for (int i = 1; i <= 9; i++) { 
+	 for (int j = 0; j <= 9; j++) {
+            for (int k = 0; k <= 9; k++) { 
+                if (i != j && j != k && i != k) {
+                    cout << i * 100 + j * 10 + k << endl;
+                    basia++;
+                }
+            }
+        }
+    }
 
-
+    cout << "Liczb bez powtarzajacych sie cyfr to :  " << basia << endl;
 
     return 0;
 }
